fb05997cf04ac621d80e3aa7db117b35e49f2f6f
f-rahim <freelancerrahim26@gmail.com>
2023-08-01 15:12:26 +0600

    Added Bookings POST API

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.undypbz.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,6 +21,7 @@ const client = new MongoClient(uri, {
 app.use(cors());
 app.use(express.json());
 
+// Base api for checking api is running
 app.get("/", (req, res) => {
   res.send("Doctors server is running");
 });
@@ -30,6 +31,9 @@ const run = async () => {
     const appointmentCollections = client
       .db("doctorsAppointment")
       .collection("appointmentCollections");
+    const bookingsCollection = client
+      .db("doctorsAppointment")
+      .collection("bookings");
 
     // get app the appointmentOptions
     app.get("/appointmentOptions", async (req, res) => {
@@ -37,7 +41,24 @@ const run = async () => {
       const result = await appointmentCollections.find(query).toArray();
       res.send(result);
     });
+
+    /**
+     * API naming convension
+     * get("/bookings")       => all bookings
+     * get("/bookings/:id")   => single booking
+     * post("/bookings")      => Post a booking
+     * patch("/bookings/:id") => Update single booking
+     * delete("/bookings/:id")=> Delete single booking
+     */
+    // Post a booking
+    app.post("/bookings", async (req, res) => {
+      const bookingInfo = req.body;
+      // console.log(bookingInfo);
+      const result = await bookingsCollection.insertOne(bookingInfo);
+      res.send(result);
+    });
   } finally {
+    // Finally
   }
 };
 run().catch(console.dir);
